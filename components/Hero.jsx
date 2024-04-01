facb75713ac697ef784ad9c2e8464dabf1545e7b
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-04-01 20:33:54 +0900

    SNSアイコン設定してスタイリングした

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,6 +2,7 @@ import { Button } from "./ui/button"
 import { Download, Send } from "lucide-react";
 import { FaArrowTurnDown } from "react-icons/fa6";
 import Link from "next/link";
+import Socials from "./Socials";
 
 
 
@@ -13,7 +14,7 @@ const Hero = () => {
                 <div className="flex justify-between gap-x-8">
                     <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
                         <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">Web Developer</div>
-                        <h1 className="h1 mb-4">Hi, My name is Naoya Yamagata</h1>
+                        <h1 className="h1 mb-4">Hi, My name is John Doe</h1>
                         <p className="subtitle max-w-[490px] mx-auto xl:mx-0">Brief description with insights into myself, my vocational journey, and what I engage in professionally</p>
                         <div className="flex flex-col gap-3 md:flex-row mx-auto xl:mx-0 mb-12">
                             <Link href="/contact">
@@ -25,6 +26,7 @@ const Hero = () => {
                                 Download CV<Download size={18} />
                             </Button>
                         </div>
+                        <Socials containerStyles={"flex gap-6 mx-auto xl:mx-0"} iconsStyles={"text-foreground text-[22px] hover:text-primary hover:scale-125 transition-all"} />
                     </div>
                     <div className="hidden xl:flex relative">image</div>
                 </div>
--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,9 +1,43 @@
+"use client";
+import { FaFacebookSquare, FaInstagram, FaGithub } from "react-icons/fa";
+import { TfiYoutube } from "react-icons/tfi";
+import { PiTwitterLogoBold } from "react-icons/pi";
+import Link from "next/link";
+
 import React from 'react'
+const icons = [
+    {
+        path: "https://github.com/Kroro1208",
+        name: <FaGithub />
+    },
+    {
+        path: "https://www.facebook.com/?locale=ja_JP",
+        name: <FaFacebookSquare />
+    },
+    {
+        path: "https://www.instagram.com/",
+        name: <FaInstagram />
+    },
+    {
+        path: "https://www.youtube.com/",
+        name: <TfiYoutube />
+    },
+    {
+        path: "https://twitter.com/?lang=ja",
+        name: <PiTwitterLogoBold />
+    },
+]
 
-const Socials = () => {
+const Socials = ({ containerStyles, iconsStyles }) => {
     return (
-        <div>
-
+        <div className={`${containerStyles}`}>
+            {icons.map((icon, index) => {
+                return (
+                    <Link href={icon.path} key={index}>
+                        <div className={`${iconsStyles}`}>{icon.name}</div>
+                    </Link>
+                )
+            })}
         </div>
     )
 }
