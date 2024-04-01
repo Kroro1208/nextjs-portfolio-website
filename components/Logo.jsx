6decb1e910d292536c3f9f8ce86a9dc4d73068f8
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-04-01 15:36:03 +0900

    Headerのスタイリングアニメーション実装した

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
 
 const Logo = () => {
     return (
-        <div>
-            Logo
-        </div>
+        <Link href="/">
+            <Image src="/logo2.svg" width={80} height={80} priority />
+        </Link>
     )
 }
 
