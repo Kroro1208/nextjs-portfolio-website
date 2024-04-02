bfdf081b4247847a7b8a4dcc85e0cb6bf8dcf881
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-04-02 21:22:21 +0900

    aboutページのデータを表示させた

--- components/About.jsx
+++ components/About.jsx
@@ -56,18 +56,18 @@ const qualificationData = [
         data: [
             {
                 company: "Meta Education",
-                qualification: "Software Engineer",
+                role: "Software Engineer",
                 years: "2004年 ~ 2007年",
             },
             {
-                company: "Meta Education",
-                qualification: "Software Engineer",
-                years: "2004年 ~ 2007年",
+                company: "Next technology",
+                role: "Software Engineer",
+                years: "2008年 ~ 20011年",
             },
             {
-                company: "Meta Education",
-                qualification: "Software Engineer",
-                years: "2004年 ~ 2007年",
+                company: "Cyber Soft",
+                role: "Software Engineer",
+                years: "20011年 ~ 2022年",
             },
         ],
     },
@@ -131,9 +131,74 @@ const About = () => {
                                                 )
                                             })}
                                         </div>
+                                        <div className="flex flex-col gap-y-2">
+                                            <div className="text-primary">Language Skill</div>
+                                            <div className="border-b border-border"></div>
+                                            <div>English, Japanese</div>
+                                        </div>
                                     </div>
                                 </TabsContent>
-                                <TabsContent value="qualification">資格</TabsContent>
+                                <TabsContent value="qualification">
+                                    <div>
+                                        <h3 className="h3 mb-8 text-center xl:text-left">
+                                            My Awesome Journey
+                                        </h3>
+                                        <div className="grid md:grid-cols-2 gap-8">
+                                            <div className="flex flex-col gap-6">
+                                                <div className="flex gap-4 items-center text-[22px] text-primary mb-4">
+                                                    <Briefcase />
+                                                    <h4 className="capitalize font-medium">
+                                                        {getData(qualificationData, 'experience').title}
+                                                    </h4>
+                                                </div>
+                                                <div className="flex flex-col gap-8 dark:text-gray-800">
+                                                    {getData(qualificationData, "experience").data.map((item, index) => {
+                                                        const { company, role, years } = item;
+                                                        return (
+                                                            <div className="flex gap-8 group" key={index}>
+                                                                <div className="h-[84px] w-[3px] bg-gray-300 relative ml-2">
+                                                                    <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px]
+                                                                    group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                                                </div>
+                                                                <div>
+                                                                    <div className="font-semibold text-lg text-muted-foreground leading-none mb-4 ">{company}</div>
+                                                                    <div className="text-lg leading-none mb-4">{role}</div>
+                                                                    <div className="text-base font-medium">{years}</div>
+                                                                </div>
+                                                            </div>
+                                                        );
+                                                    })}
+                                                </div>
+                                            </div>
+                                            <div className="flex flex-col gap-6">
+                                                <div className="flex gap-4 items-center text-[22px] text-primary mb-4">
+                                                    <GraduationCap size={28} />
+                                                    <h4 className="capitalize font-medium">
+                                                        {getData(qualificationData, 'education').title}
+                                                    </h4>
+                                                </div>
+                                                <div className="flex flex-col gap-8 dark:text-gray-800">
+                                                    {getData(qualificationData, "education").data.map((item, index) => {
+                                                        const { university, qualification, years } = item;
+                                                        return (
+                                                            <div className="flex gap-8 group" key={index}>
+                                                                <div className="h-[84px] w-[3px] bg-gray-300 relative ml-2">
+                                                                    <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px]
+                                                                    group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                                                </div>
+                                                                <div>
+                                                                    <div className="font-semibold text-lg text-muted-foreground leading-none mb-4 ">{university}</div>
+                                                                    <div className="text-lg leading-none mb-4">{qualification}</div>
+                                                                    <div className="text-base font-medium">{years}</div>
+                                                                </div>
+                                                            </div>
+                                                        );
+                                                    })}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </TabsContent>
                                 <TabsContent value="skills">スキル</TabsContent>
                             </div>
                         </Tabs>
